da3401b69bce5c09fb9cb21e191ba2dcadba4de2
karthikrudra <129721023+karthikrudra@users.noreply.github.com>
2023-05-29 13:15:01 +0530

    Update RegisterAddress.java

--- ecom.entity/src/main/java/com/feuji/ecommerce/dto/RegisterAddress.java
+++ ecom.entity/src/main/java/com/feuji/ecommerce/dto/RegisterAddress.java
@@ -1,13 +1,6 @@
 package com.feuji.ecommerce.dto;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+
 
 public class RegisterAddress {
 	
